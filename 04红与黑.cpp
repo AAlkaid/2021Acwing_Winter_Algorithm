417061bdf6de1f5ee6488514f657e943f2ab1277
AAlkaid <buzheny@gmail.com>
2021-02-04 23:26:10 +0800

    update04

--- 04红与黑.cpp
+++ 04红与黑.cpp
@@ -14,6 +14,9 @@ const int N = 25;
 int n, m;
 char g[N][N];
 
+int dx[] = {-1, 0, 1, 0}, dy[] = {0, 1, 0, -1};
+
+
 int bfs(int sx, int sy)
 {
 	queue<PII> q;
@@ -22,7 +25,6 @@ int bfs(int sx, int sy)
 	g[sx][sy] = '#';
 	int res = 0;
 
-	int dx[] = {-1, 0, 1, 0}, dy[] = {0, 1, 0, -1};
 
 	while (q.size())
 	{
@@ -43,6 +45,18 @@ int bfs(int sx, int sy)
 	return res;
 }
 
+int dfs(int x, int y)
+{
+	int res = 1;
+	g[x][y] = '#';
+	for (int i = 0; i < 4; i ++)
+	{
+		int a = x + dx[i], b = y + dy[i];
+		if (a >= 0 && a < n && b >= 0 && b < m && g[a][b] == '.' )
+			res += dfs(a, b);
+	}
+	return res;
+}
 
 
 int main(){
@@ -62,7 +76,7 @@ int main(){
 					x = i;
 					y = j;
 				}
-		cout << bfs(x,y) << endl;
+		cout << dfs(x,y) << endl;
 	}
 	return 0;
 }
